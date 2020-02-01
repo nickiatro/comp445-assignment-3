b12a41a929a096138574d8ffc00389324f6a56bb
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2020-01-31 20:54:51 -0500

    Added getOperation, postOperation and main methods

--- src/HttpClient.java
+++ src/HttpClient.java
@@ -1,4 +1,17 @@
+import java.net.Socket;
 
 public class HttpClient {
 
+	public void getOperation() {
+	
+	}
+	
+	public void postOperation() {
+		
+	}
+	
+	public static void main(String[] args) {
+	
+	}
+	
 }
