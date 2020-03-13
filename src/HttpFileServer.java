820e47df0a18a34a61b893dac0b83b17546ad615
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2020-03-12 23:36:27 -0400

    removed unused import

--- src/HttpFileServer.java
+++ src/HttpFileServer.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
